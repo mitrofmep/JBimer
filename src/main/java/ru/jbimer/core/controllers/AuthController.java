f89d271994cdf805575cad1adc6c04908b6dfd93
mitrofmep <mitrofmep@gmail.com>
2023-04-27 01:03:18 +0800

    refactor: add beans of Spring Security 5, new login form

---
+++ src/main/java/ru/jbimer/core/controllers/AuthController.java
@@ -0,0 +1,15 @@
+package ru.jbimer.core.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "auth/login";
+    }
+}
